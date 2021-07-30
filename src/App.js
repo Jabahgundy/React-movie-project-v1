aae118a7915079a2efe85db2f0d64636af855452
Jabah Gundy <gundy157@outlook.com>
2021-07-30 03:11:04 -0400

    updated project

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListTitle from './components/MovieListTitle';
 import Search from './components/Search';
+import AddFavorites from './components/AddFavorites';
+import RemoveFavorites from './components/RemoveFavorites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+
+
+  const [favorites, setFavorites] = useState([]);
 
   // when user SEARCH the input stays current //
   // ONCE we PASS in the VALUES, users we see results //
@@ -36,7 +41,36 @@ const App = () => {
     // updates when user searches for a movie //
   }, [searchVal]);
 
-  // return is when you render the info you want users to see //
+  useEffect(() => {
+    const movieFavorites = JSON.parse(localStorage.getItem('react-movie-app-favorites')
+    );
+    setFavorites(movieFavorites);
+  }, []);
+
+
+  // this is how to save items to local storage //
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favorites', JSON.stringify(items))
+  }
+
+  // Here's the function to update the movie list array//
+  const AddFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  }
+  // Function to remove from the array of movies //
+  const RemoveFavoritesMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorite) => favorite.imdbID !== movie.imdbID
+    );
+
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  }
+
+  // RETURN is when you render the info you want users to see //
+
   return (
     <div className='container-fluid movie-app'>
       <div className='col d-flex align-items-center mt-4 mb-4'>
@@ -44,9 +78,21 @@ const App = () => {
         {/* SEARCH VALUES */}
         <Search searchVal={searchVal} setSearchVal={setSearchVal} />
       </div>
+      {/* Add favorites function */}
+      <div className='col row'>
+        <MovieList movies={movies}
+          handleFavoritesClick={AddFavoriteMovie}
+          FavoritesComponent={AddFavorites} />
+      </div>
 
-      <div className='row'>
-        <MovieList movies={movies} />
+      <div className='col d-flex align-items-center mt-4 mb-4'>
+        <MovieListTitle title='Favorites' />
+      </div>
+      {/* remove favorites function */}
+      <div className='col row'>
+        <MovieList movies={favorites}
+          handleFavoritesClick={RemoveFavoritesMovie}
+          FavoritesComponent={RemoveFavorites} />
       </div>
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const MovieList = (props) => {
+    const FavoritesComponent = props.FavoritesComponent;
     return (
         <>
             {props.movies.map((movie, index) =>
                 <div className='col image-container d-flex justify-content-start'>
                     <img src={movie.Poster} alt='movie'></img>
-                    <div className='overlay d-flex align-items-center justify-content-center'></div>
+                    <div
+                        onClick={() => props.handleFavoritesClick(movie)}
+                        className='overlay d-flex align-items-center col justify-content-center'>
+                        <FavoritesComponent />
+                    </div>
                 </div>
             )}
         </>
